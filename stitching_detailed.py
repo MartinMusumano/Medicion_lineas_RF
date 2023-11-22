72fd0448613f7789c8e884b52f5b8bbb1b5083b6
MartinMusumano <martinmusumano@gmail.com>
2023-11-22 09:11:38 -0300

    Imagen original añadida

--- stitching_detailed.py
+++ stitching_detailed.py
@@ -7,64 +7,6 @@ python-opencv > 4.0.1
 https://docs.opencv.org/4.x/d8/d19/tutorial_stitcher.html
 https://github.com/opencv/opencv/blob/4.x/samples/python/stitching_detailed.py
 
-Positional arguments:
-  img_names             Files to stitch
-
-Options:
-  -h, --help            show this help message and exit
-  --try_cuda TRY_CUDA   Try to use CUDA. The default value is no. All default values are for CPU mode.
-  --work_megapix WORK_MEGAPIX
-                        Resolution for image registration step. The default is 0.6 Mpx
-  --features {orb,sift,brisk,akaze}
-                        Type of features used for images matching. The default is 'orb'.
-  --matcher {homography,affine}
-                        Matcher used for pairwise image matching. The default is 'homography'.
-  --estimator {homography,affine}
-                        Type of estimator used for transformation estimation. The default is 'homography'.
-  --match_conf MATCH_CONF
-                        Confidence for feature matching step. The default is 0.3 for ORB and 0.65 for other feature
-                        types.
-  --conf_thresh CONF_THRESH
-                        Threshold for two images are from the same panorama confidence.The default is 1.0.
-  --ba {ray,reproj,affine,no}
-                        Bundle adjustment cost function. The default is 'ray'.
-  --ba_refine_mask BA_REFINE_MASK
-                        Set refinement mask for bundle adjustment. It looks like 'x_xxx', where 'x' means refine
-                        respective parameter and '_' means don't refine, and has the following
-                        format:<fx><skew><ppx><aspect><ppy>. The default mask is 'xxxxx'. If bundle adjustment doesn't
-                        support estimation of selected parameter then the respective flag is ignored.
-  --wave_correct {horiz,no,vert}
-                        Perform wave effect correction. The default is 'horiz'
-  --save_graph SAVE_GRAPH
-                        Save matches graph represented in DOT language to <file_name> file.
-  --warp {spherical,plane,affine,cylindrical,fisheye,stereographic,compressedPlaneA2B1,compressedPlaneA1.5B1,compressedPlanePortraitA2B1,
-          compressedPlanePortraitA1.5B1,paniniA2B1,paniniA1.5B1,paniniPortraitA2B1,paniniPortraitA1.5B1,mercator,transverseMercator}
-                        Warp surface type. The default is 'spherical'.
-  --seam_megapix SEAM_MEGAPIX
-                        Resolution for seam estimation step. The default is 0.1 Mpx.
-  --seam {gc_color,gc_colorgrad,dp_color,dp_colorgrad,voronoi,no}
-                        Seam estimation method. The default is 'gc_color'.
-  --compose_megapix COMPOSE_MEGAPIX
-                        Resolution for compositing step. Use -1 for original resolution. The default is -1
-  --expos_comp {gain_blocks,gain,channel,channel_blocks,no}
-                        Exposure compensation method. The default is 'gain_blocks'.
-  --expos_comp_nr_feeds EXPOS_COMP_NR_FEEDS
-                        Number of exposure compensation feed.
-  --expos_comp_nr_filtering EXPOS_COMP_NR_FILTERING
-                        Number of filtering iterations of the exposure compensation gains.
-  --expos_comp_block_size EXPOS_COMP_BLOCK_SIZE
-                        BLock size in pixels used by the exposure compensator. The default is 32.
-  --blend {multiband,feather,no}
-                        Blending method. The default is 'multiband'.
-  --blend_strength BLEND_STRENGTH
-                        Blending strength from [0,100] range. The default is 5
-  --output OUTPUT       The default is 'result.jpg'
-  --timelapse TIMELAPSE
-                        Output warped images separately as frames of a time lapse movie, with 'fixed_' prepended to
-                        input file names.
-  --rangewidth RANGEWIDTH
-                        uses range_width to limit number of images to match with.
-
 """
 
 # Python 2/3 compatibility
